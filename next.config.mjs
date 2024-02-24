9f23323dc436f004adfe6976b668ba3b81448a59
shobhit <shobhit.goyal@carefi.in>
2024-02-25 00:55:18 +0530

    Delete unused files and update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,10 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
-	output: "export",
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
 };
 
 export default withContentlayer(nextConfig);
